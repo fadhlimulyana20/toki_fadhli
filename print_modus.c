e0b05b4bb7376d91232fd1cf5fe57eac1573f260
fadhlimulyana20 <baba.fadhli@gmail.com>
2019-10-14 20:29:23 +0700

    Udah jadi 100 di TOKI gegara counter[i] > greatest buat ngetes kalau modusnya lebih kecil dari modus terbesar di else if.

--- print_modus.c
+++ print_modus.c
@@ -20,32 +20,17 @@ int main(){
             }
             rep--;
         }
-        
-        // for (int j = rep; j > 0; j--)
-        // {
-        //     if (arr[i] == arr[j-1] && counter[j-1] != 0)
-        //     {
-        //         counter[i] = counter[j-1]++;
-        //     }else
-        //     {
-        //         counter[i] = 1;
-        //     }    
-        // }
     
         if (counter[i] >= greatest && arr[i] >= mode)
         {
             greatest = counter[i];
             mode = arr[i];
-        }else if (counter[i] >= greatest && arr[i] <= mode)
+        }else if (counter[i] > greatest && arr[i] <= mode)
         {
             greatest = counter[i];
             mode = arr[i];
         }    
     }
-    // for (int i = 0; i < N; i++)
-    // {
-    //     printf("%d ", counter[i]);
-    // }
     
     printf("%d\n", mode);
 }
